fc4ba66f4a5869bad5360b6ce4e24a751fe63169
Nikhil Patnaik <91794081+nkhlptnk@users.noreply.github.com>
2026-07-07 12:12:05 +0530

    Update API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://hragent-production-f7c1.up.railway.app/',
+  baseURL: 'http://localhost:8001/api',
   headers: {
     'Content-Type': 'application/json',
   },
